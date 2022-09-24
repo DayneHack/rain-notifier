4fb2301c058a9f6dd2ddd142b29bde06ed0dda81
Steve-07 <55524682+Steve-07@users.noreply.github.com>
2022-09-24 14:47:42 -0700

    api returns info about rain for single city

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
 const key = config.MY_KEY
-const city = 'vancouver'
 const locationKey = '53286'
-
-function startFunc(){
+//54488 Hopedale
+//53286 vancouver
+async function startFunc(){
 
     var x = document.getElementById("resultDiv");
-    data = getWeatherData();
-    //rainChance = determineRain(data);
-    x.innerHTML = rainChance;
+    var y = document.getElementById("rainChanceDiv");
+
+    let data = getWeatherData();
+    let result = await determineRain(data);
+
+    if (result.itWillRain){
+        x.innerHTML = "It will rain today";
+        y.innerHTML = ""
+    }
+    else{
+        x.innerHTML = "It likely will not rain today";
+        y.innerHTML = "Chance of rain today: " + parseInt(result.rainChance)/10 + "%"
+    }
 }
 
 async function getWeatherData(){
 
     var base = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/";
-    var end = "53286";
     var apikey = `?apikey=${key}`;
-    var call = await fetch(base + end + apikey);
+    var call = await fetch(base + locationKey + apikey);
     var data = await call.json();
 
-    return determineRain(data)
+    return data
 }
 
-function determineRain(data){
+async function determineRain(data){
 
-    console.log(data)
-
+    data = await data;
+    var rainChance = 0;
+    console.log(data);
+    for (let i = 0; i < 12; i++){
+        if (data[i].HasPrecipitation){
+            var itWillRain = true;
+            break;
+        }
+        else{
+            if (data[i].PrecipitationProbability > rainChance){
+                rainChance = data[i].PrecipitationProbability;
+            }
+        }
+    }
+    return {itWillRain, rainChance}
 }
