84fb30ed2d795c8c4ec229150e8f4f31618df389
Steve-07 <55524682+Steve-07@users.noreply.github.com>
2022-09-29 14:52:32 -0700

    add prefix to api call link

--- main.js
+++ main.js
@@ -34,9 +34,10 @@ async function startFunc(){
 async function getWeatherData(locationKey){
 
     locationKey = await locationKey
-    var base = "https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/";
+    var pre = "https://cors-anywhere.herokuapp.com/corsdemo"
+    var base = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/";
     var apikey = `?apikey=${key}`;
-    var call = await fetch(base + locationKey + apikey + "&metric=true");
+    var call = await fetch(pre + base + locationKey + apikey + "&metric=true");
     var data = await call.json();
     console.log(data)
 
@@ -63,10 +64,11 @@ async function determineRain(data){
 
 async function getLocationId(text){
 
+    var pre = "https://cors-anywhere.herokuapp.com/corsdemo"
     var base = "https://dataservice.accuweather.com/locations/v1/cities/search";
     var apikey = `?apikey=${key}&q=${text}`;
 
-    var call = await fetch(base + apikey);
+    var call = await fetch(pre + base + apikey);
     var id = await call.json();
 
     return id;
